8c6582d086c659c97bbd1852e77b50fdbe2099c0
Sahil <sahilmohankudo111@gmail.com>
2023-04-04 19:28:17 +0530

    Updates

---
+++ precache-manifest.8a60c4400644f23deae6e95c6d3f34ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddeb303f48e3cbd490aa",
+    "url": "/TextUtils-React/static/css/main.2c289a92.chunk.css"
+  },
+  {
+    "revision": "ddeb303f48e3cbd490aa",
+    "url": "/TextUtils-React/static/js/main.b94831dc.chunk.js"
+  },
+  {
+    "revision": "bee45f32852a404381b2",
+    "url": "/TextUtils-React/static/js/runtime~main.9c336662.js"
+  },
+  {
+    "revision": "ae4e00779f8dfe0d96ce",
+    "url": "/TextUtils-React/static/js/2.fa466411.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/TextUtils-React/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtils-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fcb18cdb0591351a3ad3aa4aa2619a6c",
+    "url": "/TextUtils-React/index.html"
+  }
+];
